da54523c164cf80bfc27e8efee5b77bdc3a0f91e
ItzelSilMc <isilva@visualmexico.com.mx>
2025-10-06 15:45:49 -0600

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,51 @@
-import { Component, HostListener } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements  AfterViewInit  {
+   
+ @ViewChild('audioRef', { static: false }) audioElement!: ElementRef<HTMLAudioElement>;
+  private played = false; // para que solo se reproduzca una vez
+
+  constructor(private renderer: Renderer2) {}
+
+  ngAfterViewInit() {
+    // Crear listener global para click o touch
+     let played = false;
+
+  const audio = this.audioElement.nativeElement;
+
+  // Listener para click
+  const removeClickListener = this.renderer.listen('document', 'click', () => {
+    if (!played) {
+      audio.src = 'assets/musica/miCancion.mp3';
+      audio.play().catch(err => console.log('Error al reproducir audio:', err));
+      played = true;
+
+      // Remueve listener después del primer toque
+      removeClickListener();
+      removeTouchListener();
+    }
+  });
+
+  // Listener para touchstart (móviles)
+  const removeTouchListener = this.renderer.listen('document', 'touchstart', () => {
+    if (!played) {
+      audio.src = 'assets/Musica/cancion.mp3';
+      audio.play().catch(err => console.log('Error al reproducir audio:', err));
+      played = true;
+
+      // Remueve listener después del primer toque
+      removeClickListener();
+      removeTouchListener();
+    }
+  });
+  }
+
+  /*
   title = 'mi-invitacion';
    textoTransform: string = 'translateY(0px)';
   textoOpacity: number = 0;
@@ -20,4 +60,5 @@ export class AppComponent {
     // Opacidad del texto
     this.textoOpacity = Math.min(1, scrollY / 300);
   }
+    */
 }
